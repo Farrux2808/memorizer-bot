36f4c3c292aaf2365fd769e4a85fa7395c65cb00
Farrux <farruxxolikulov@gmail.com>
2023-04-04 17:32:21 +0500

    'fixed'

--- scheduler ticket.py
+++ scheduler ticket.py
@@ -4,21 +4,24 @@ import telegram
 import requests
 import datetime
 
+FarruxId = 1571963948
+JovidId = 156754695
+GroupId = -843964106
 bot = telegram.Bot(token="5210098659:AAEeJTWsjl_j9MyL598eR2iHXYLWieqwWag")
-def job():
+def job(depDate, stationFrom, stationTo):
     date = datetime.datetime.now()
     try:
         print('stated:', date)
         Data = {
             "direction": [
                 {
-                    "depDate": "24.02.2023",
+                    "depDate": depDate,
                     "fullday": True,
                     "type": "Forward"
                 }
             ],
-            "stationFrom": "2900000",
-            "stationTo": "2900700",
+            "stationFrom": stationFrom,
+            "stationTo": stationTo,
             "detailNumPlaces": 1,
             "showWithoutPlaces": 0
         }
@@ -31,17 +34,36 @@ def job():
         fromPlace = tickets['express']['direction'][0]['passRoute']['from']
         toPlace = tickets['express']['direction'][0]['passRoute']['to']
         for ticket in tickets['express']['direction'][0]['trains'][0]['train']:
-            if (ticket['brand'] == 'Sharq' or ticket['brand'] == 'Afrosiyob') and (ticket['departure']['time'].split(':')[0] == '19'):
+            if  ticket['departure']['time'].split(':')[0][0] == '0' and ticket['brand'] == 'Afrosiyob':
                 if len(ticket['places']['cars']) > 0:
                     for place in ticket['places']['cars']:
-                        if place['indexType'] != '5':
-                            bot.send_message(chat_id = 1571963948, 
+                        if place['indexType'] != '5' or True:
+                            bot.send_message(chat_id = GroupId, 
                             text = f"""🎫Bilet: {ticket['brand']} {fromPlace} dan {toPlace} ga
 🕔Vaqti: {ticket['departure']['time']} {ticket['departure']['localDate']}
 😎Class: {place['typeShow']}""")
     except:
         print('errored:', date)
-schedule.every(1).minutes.do(job)
+
+
+
+def runer():
+    t = [
+        {
+            "depDate": "16.04.2023",
+            "stationFrom": "2900700",
+            "stationTo": "2900000",
+        },
+        {
+            "depDate": "09.04.2023",
+            "stationFrom": "2900700",
+            "stationTo": "2900000",
+        }
+    ]
+    for i in t:
+        job(depDate=i["depDate"], stationFrom=i['stationFrom'], stationTo=i['stationTo'])
+        time.sleep(15)
+schedule.every(1).minutes.do(runer)
 
 while True:
     schedule.run_pending()
